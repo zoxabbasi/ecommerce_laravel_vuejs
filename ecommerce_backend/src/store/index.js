3a92e4a541cc587203825697202a6ec6f4157575
Talal Abbasi <zoxabbasi@gmail.com>
2023-05-29 19:02:26 +0500

    Added login form

---
+++ ecommerce_backend/src/store/index.js
@@ -0,0 +1,12 @@
+import {createStore} from "vuex";
+
+const store = createStore({
+    state : {
+        test: '1234567'
+    },
+    getter : {},
+    actions : {},
+    mutations: {},
+});
+
+export default store
